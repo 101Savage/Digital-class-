9c0c42a124ee7f9127b2eb871225c4f0d75f9fbd
101Savage <edehc7601#@gmail.com>
2025-01-21 10:51:00 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+// console.log("turn 2 fan!")
+// console.log("turn 2 fan!")
+// console.log("turn 2 fan!")
+// console.log("turn 2 fan!")
+// console.log("turn 2 fan!")
+// console.log("turn 2 fan!")
+
+var message ='turn our fan!'
+let num = 7
+const add = num + 3
+const mult = num * 5
+const sub = num - 8
+
+console.log(message)
+console.log(message)
+console.log(message)
+console.log(message)
+console.log(message)
+console.log(message)
+
+// console.log(add)
+
+
+// let remender = 10 / 2
+// console.log(remender, 10, 30, 12)
+
+// let number = 5
+// let number2 = 10
+
+// let name1 = "obi "
+// let name2 = "peter "
+
+// let fullname = name1 + name2
+// console.log(fullname)
+
+// let num1 = 4
+// let num2 = 10
+// let num3 = 10
+
+// console.log(num1 + num2 + num3)
+
+// function showMessage(){ 
+    
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// }
+
+// showMessage()
+
+// showMessage()
+
+
+function addnums(){
+    let num1 = 5
+}

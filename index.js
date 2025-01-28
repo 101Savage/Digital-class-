ca6aeab2bfecf0366e3459c032e14207c2112a50
101Savage <edehc7601#@gmail.com>
2025-01-28 10:22:21 +0100

    salt

--- index.js
+++ index.js
@@ -5,18 +5,18 @@
 // console.log("turn 2 fan!")
 // console.log("turn 2 fan!")
 
-var message ='turn our fan!'
-let num = 7
-const add = num + 3
-const mult = num * 5
-const sub = num - 8
+// var message ='turn our fan!'
+// let num = 7
+// const add = num + 3
+// const mult = num * 5
+// const sub = num - 8
 
-console.log(message)
-console.log(message)
-console.log(message)
-console.log(message)
-console.log(message)
-console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
 
 // console.log(add)
 
@@ -54,6 +54,82 @@ console.log(message)
 // showMessage()
 
 
-function addnums(){
-    let num1 = 5
-}
+// function addnums(){
+//     let num1 = 5
+// }
+
+// function addmult(num1, num2, str) {
+//     let add = num1 + num2
+//     let result = add * 10
+//     let output = str + ' ' + result
+//     return result
+// }
+
+// let result = addmult(10, 10, 'the result is')
+// console.log(result * 10)
+
+// let car1 = {name: 'camry', model: 2015, tyer: 4, color: 'black', price: '$3500'}
+// let car2 = {
+//     name: 'supra', 
+//     model: 2022, 
+//     tyer: 4, 
+//     color: 'black', 
+//     price: '$35000',
+//     move: function (){
+
+//     console.log('starting...........')
+//     console.log('moving................')
+//     return 'wow i can finaly move'
+// }
+// }
+
+
+// console.log(car2)
+
+// let car3 ={}
+// car3.name = 'toyota'
+// car3.model = 2020
+// car3.tyer = 4
+// car3.color = 'gery'
+// car3.price = '$5600'
+
+// console.log(car3)
+// let values = Object.values(car3)
+// let props = Object.keys(car3)
+// console.log(values)
+// console.log(props)
+
+// let str = 'asshjhkhkrbwbubwyUBZNBSUUHWUEUHDUHHIASJDHIADKAIIAHIFHBHUZHI'
+// let sentence = ' i am ... atomic '
+// let position = sentence
+// console.log(sentence.length)
+
+// console.log(str.slice(5, 21))
+// console.log(str.substring(5, 21))
+// console.log(str.substr(5, 21))
+
+let num11 = 200
+// let string = `Who how are u? ${sentence} ${num11}`
+// console.log(string)
+
+
+let numstr = num11.toString()
+console.log(numstr)
+
+let decimalnum = 10.399967750
+let decimal = decimalnum.toFixed(2)
+console.log(number(decimal) + 30)
+
+let myarray = ['camry', 'blue', 4, 2024, 2000]                                                                 
+console.log(myarray)
+console.log(myarray[0])
+console.log(myarray[1])
+myarray.push('toyota')
+myarray.push('red')
+myarray.push(2020)
+myarray.push(22000)
+console.log(myarray)
+
+
+
+
